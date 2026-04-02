55b3ba0ea1b85b4e1ebc509a1de92a0ed70dc7f5
PaulJ74 <p.janssen@choretime.nl>
2026-04-02 08:50:25 +0200

    Delete replit.nix

--- replit.nix
+++
@@ -1,9 +0,0 @@
-run = "npm run dev"
-entrypoint = "src/main.jsx"
-
-[nix]
-channel = "stable-22_11"
-
-[deployment]
-run = ["sh", "-c", "npm run build && npm run preview"]
-deploymentTarget = "cloudrun"
